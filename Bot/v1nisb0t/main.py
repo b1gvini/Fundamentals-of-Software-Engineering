8c91cb3cd351596904cf66a80c782056f0447952
Vinícius de França <vgf.vinicius@gmail.com>
2019-08-27 05:24:50 -0300

    Adicionado o Bot

---
+++ Bot/v1nisb0t/main.py
@@ -0,0 +1,15 @@
+import telepot
+from Chatbot import Chatbot
+
+from Chatbot import Chatbot
+
+Bot = Chatbot('Felipe')
+while True:
+    frase = Bot.escuta()
+    resp = Bot.pensa(frase)
+    Bot.fala(resp)
+    if resp == 'tchau':
+        break
+
+
+
